fc54404bc4f77d90335cda5836530512c4c73328
Renatogranados <renatogranadosgt@gmail.com>
2022-05-22 22:58:25 -0600

    feat: add statistics and about

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -1,10 +1,57 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import * as Linking from 'expo-linking';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function About() {
+  const GITHUB = 'https://github.com/renato6gs';
+  const LINKEDIN = 'https://www.linkedin.com/in/renato-granados-636935233/';
+
+  const goTo = ({ url }) => {
+    Linking.openURL(url);
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Acerca de</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Acerca de</Text>
+      <View style={styles.informationContainer}>
+        <Text style={styles.description}>
+          Aplicación desarrollada en React Native para el curso de Compiladores de la Universidad Mariano Gálvez de
+          Guatemala
+        </Text>
+      </View>
+      <View style={styles.linksContainer}>
+        <TouchableOpacity style={styles.link} onPress={() => goTo({ url: LINKEDIN })}>
+          <Ionicons name='logo-linkedin' size={32} color='#7F1431' />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.link} onPress={() => goTo({ url: GITHUB })}>
+          <Ionicons name='logo-github' size={32} color='#7F1431' />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 20,
+    paddingHorizontal: 15,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  informationContainer: {
+    marginVertical: 20,
+  },
+  description: {
+    fontSize: 16,
+    lineHeight: 27,
+  },
+  linksContainer: {
+    flexDirection: 'row',
+  },
+  link: {
+    marginLeft: 10,
+  },
+});
--- src/screens/StatisticsScreen.jsx
+++ src/screens/StatisticsScreen.jsx
@@ -1,10 +1,95 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { Text, View, StyleSheet, ScrollView } from 'react-native';
+import ContentHeader from '../components/ContentHeader.jsx';
+import { useFocusEffect } from '@react-navigation/native';
+import * as SQLite from 'expo-sqlite';
 
 export default function StatisticsScreen() {
+  const [miss, setMiss] = useState(0);
+  const [obtain, setObtain] = useState(0);
+  const [repeat, setRepeat] = useState(0);
+
+  const getData = ({ data }) => {
+    let obtain = 0;
+    let miss = 0;
+    let repeat = 0;
+
+    data.forEach((item) => {
+      console.log(item);
+      const { times } = item;
+
+      if (times === 0) miss++;
+      else if (times === 1) obtain++;
+      else if (times > 1) repeat++;
+    });
+
+    setMiss(miss);
+    setObtain(obtain);
+    setRepeat(repeat);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      try {
+        const db = SQLite.openDatabase('db.db');
+        db.transaction((tx) => {
+          tx.executeSql('select * from cards', [], (_, { rows }) => {
+            console.log(JSON.stringify(rows));
+            console.log('Sin stringify');
+            console.log(rows);
+            const { _array } = rows;
+            console.log('array es');
+            console.log(_array);
+            // setData(_array);
+            getData({ data: _array });
+          });
+        }, null);
+      } catch (error) {
+        console.log('hubo un error:');
+        console.log(error);
+      }
+    }, [])
+  );
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Estadísticas</Text>
-    </View>
+    <ScrollView>
+      <ContentHeader />
+      <View style={styles.container}>
+        <View style={[styles.statisticContainer, styles.obtainContainer]}>
+          <Text style={styles.text}>Figuritas obtenidas: {obtain}</Text>
+        </View>
+        <View style={[styles.statisticContainer, styles.repeatContainer]}>
+          <Text>Figuritas repetidas: {repeat}</Text>
+        </View>
+        <View style={[styles.statisticContainer, styles.missContainer]}>
+          <Text>Figuritas faltantes: {miss}</Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 20,
+    paddingHorizontal: 15,
+  },
+  statisticContainer: {
+    paddingVertical: 25,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    marginBottom: 30,
+  },
+  obtainContainer: {
+    backgroundColor: '#3D7773',
+  },
+  repeatContainer: {
+    backgroundColor: '#F9BC4C',
+  },
+  missContainer: {
+    backgroundColor: '#C4C4C4',
+  },
+  text: {
+    color: '#fff',
+  },
+});
